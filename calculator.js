ae97ea38d236f2338cec25b6d2ef9ea1ff40d02f
Kilbane <kilbanen@tcd.ie>
2020-09-25 20:47:11 +0100

    Cancel button now works

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
-let expression = "";
-
 function insert(button){
   let oldExpression = document.getElementsByClassName('display')[0].innerHTML;
   let newExpression = oldExpression.concat(button.innerHTML);
   document.getElementsByClassName("display")[0].innerHTML = newExpression;
+}
+
+function cancel(){
+  document.getElementsByClassName("display")[0].innerHTML = "0";
 }
