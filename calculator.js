c312dcca7c89f2f42e31a4d368e738a92b7bed64
Kilbane <kilbanen@tcd.ie>
2020-09-25 21:09:48 +0100

    Removed leading 0 for expressions

--- calculator.js
+++ calculator.js
@@ -1,9 +1,13 @@
+let displayIsCleared = true;
+
 function insert(button){
-  let oldExpression = document.getElementsByClassName('display')[0].innerHTML;
+  let oldExpression = displayIsCleared ? "" : document.getElementsByClassName('display')[0].innerHTML;
   let newExpression = oldExpression.concat(button.innerHTML);
   document.getElementsByClassName("display")[0].innerHTML = newExpression;
+  displayIsCleared = false;
 }
 
 function cancel(){
   document.getElementsByClassName("display")[0].innerHTML = "0";
+  displayIsCleared = true;
 }
